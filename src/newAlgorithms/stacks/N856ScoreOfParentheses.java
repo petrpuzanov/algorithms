3b18132732a89927fa1724bb6f3d340e0731efb2
Petr Puzanov <peta12321@gmail.com>
2026-03-27 13:33:08 +0900

    N856ScoreOfParentheses.java

---
+++ src/newAlgorithms/stacks/N856ScoreOfParentheses.java
@@ -0,0 +1,36 @@
+package newAlgorithms.stacks;
+
+import java.util.Stack;
+
+public class N856ScoreOfParentheses {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(0); // маркер начала
+            } else {
+                int val = 0;
+
+                while (stack.peek() != 0) {
+                    val += stack.pop();
+                }
+
+                stack.pop(); // убираем 0
+
+                if (val == 0) {
+                    stack.push(1); // "()"
+                } else {
+                    stack.push(2 * val); // "(A)"
+                }
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
